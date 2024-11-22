b547da52a47fc48fa4067072e0465a5d9cd2df79
Ethan Holz <ethan.holz@omsf.io>
2024-11-22 11:30:30 -0700

    feat: add a default for the github timeout

--- src/start_aws_gha_runner/__main__.py
+++ src/start_aws_gha_runner/__main__.py
@@ -5,41 +5,40 @@ from gha_runner.clouddeployment import DeployInstance
 import os
 
 
-def main():
+def check_required_env_vars():
     required = ["GH_PAT", "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"]
     for req in required:
         if req not in os.environ:
             raise Exception(f"Missing required environment variable {req}")
 
-    # Set the default timeout to 20 minutes
-    timeout = os.environ.get("INPUT_GH_TIMEOUT")
-    gh_timeout = 1200
-    if timeout is not None and timeout != "":
-        gh_timeout = int(timeout)
+def main():
+    # Check that everything exists
+    check_required_env_vars()
+    # The timeout is infallible
+    timeout = int(os.environ["INPUT_GH_TIMEOUT"])
+    # timeout = os.environ.get("INPUT_GH_TIMEOUT")
+    # gh_timeout = 1200
+    # if timeout is not None and timeout != "":
+    #     gh_timeout = int(timeout)
 
-    gha_params = {
-        "token": os.environ["GH_PAT"],
-    }
-    repo = os.environ.get("INPUT_REPO")
-    if repo is None or repo == "":
-        repo = os.environ.get("GITHUB_REPOSITORY")
-    # We check again to validate that this was set correctly
-    if repo is not None or repo == "":
-        gha_params["repo"] = repo
-    else:
-        raise Exception("Repo key is missing or GITHUB_REPOSITORY is missing")
+    token = os.environ["GH_PAT"]
+    # If the repo is not set, use the calling repo
+    calling_repo = os.environ["GITHUB_REPOSITORY"]
+    repo = os.environ.get("INPUT_REPO", calling_repo)
+
+    # This is infallable because it has a default value
     instance_count = int(os.environ["INPUT_INSTANCE_COUNT"])
 
     aws_params = parse_aws_params()
-    aws_params["repo"] = gha_params["repo"]
-    gh = GitHubInstance(token=os.environ["GH_PAT"], repo=gha_params["repo"])
+    aws_params["repo"] = repo
+    gh = GitHubInstance(token=token, repo=repo)
     # This will create a new instance of StartAWS and configure it correctly
     deployment = DeployInstance(
         provider_type=StartAWS,
         cloud_params=aws_params,
         gh=gh,
         count=instance_count,
-        timeout=gh_timeout,
+        timeout=timeout,
     )
     # This will output the instance ids for using workflow sytnax
     deployment.start_runner_instances()
